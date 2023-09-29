e383440d80bca409455cc1fefa8cfdc4e4b76c9e
matibonura <30848890+matibonura@users.noreply.github.com>
2023-09-29 12:39:43 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+console.log("hola")
+document.addEventListener("DOMContentLoaded", function() {
+    var hamburger = document.querySelector(".hamburger");
+    hamburger.onclick = function () {
+        var nav = document.querySelector(".nav");
+        var ul = nav.querySelector("ul");
+        ul.classList.toggle("active");
+            
+        // También podemos agregar "active" a los elementos <li> dentro de <ul>
+        var lis = ul.querySelectorAll("li");
+        lis.forEach(function(li) {
+            li.classList.toggle("active");
+        });
+        }
+})
